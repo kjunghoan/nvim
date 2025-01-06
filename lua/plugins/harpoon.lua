3958551a67eab44a423f26cd5a42b7509234da08
Jung Hoan Kim <kjunghoan@gmail.com>
2025-01-05 23:23:40 -0500

    Updated to harpoon to align with current version specifications

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -6,21 +6,86 @@ return {
   },
   config = function()
     local harpoon = require("harpoon")
-    harpoon:setup()
-
-    local wk = require("which-key")
-    wk.add({
-      { "<leader>h", group = "Harpoon" },
-      { "<leader>ha", function() harpoon:list():add() end, desc = "Add File" },
-      { "<leader>he", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "Toggle Menu" },
-      { "<leader>h1", function() harpoon:list():select(1) end, desc = "File 1" },
-      { "<leader>h2", function() harpoon:list():select(2) end, desc = "File 2" },
-      { "<leader>h3", function() harpoon:list():select(3) end, desc = "File 3" },
-      { "<leader>h4", function() harpoon:list():select(4) end, desc = "File 4" },
+    
+    -- Set up harpoon with a basic configuration
+    harpoon:setup({
+      settings = {
+        save_on_toggle = true,
+        sync_on_ui_close = true,
+        key = function()
+          return vim.loop.cwd()
+        end
+      }
     })
 
-    -- Additional navigation keymaps if you want them
-    vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-    vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+    -- Create a which-key prefix group for Harpoon
+    local wk = require("which-key")
+    wk.register({
+      ["<leader>h"] = { name = "Harpoon", _ = "which_key_ignore" },
+    })
+
+    -- Register the keymaps
+    wk.register({
+      ["<leader>ha"] = {
+        function()
+          harpoon:list():append()
+        end,
+        "Harpoon Add File"
+      },
+      ["<leader>he"] = {
+        function()
+          local conf = require("telescope.config").values
+          local function toggle_telescope(harpoon_files)
+            local file_paths = {}
+            for _, item in ipairs(harpoon_files.items) do
+              table.insert(file_paths, item.value)
+            end
+
+            require("telescope.pickers").new({}, {
+              prompt_title = "Harpoon",
+              finder = require("telescope.finders").new_table({
+                results = file_paths,
+              }),
+              previewer = conf.file_previewer({}),
+              sorter = conf.generic_sorter({}),
+            }):find()
+          end
+          toggle_telescope(harpoon:list())
+        end,
+        "Harpoon Quick Menu"
+      },
+      ["<leader>h1"] = {
+        function()
+          harpoon:list():select(1)
+        end,
+        "Harpoon File 1"
+      },
+      ["<leader>h2"] = {
+        function()
+          harpoon:list():select(2)
+        end,
+        "Harpoon File 2"
+      },
+      ["<leader>h3"] = {
+        function()
+          harpoon:list():select(3)
+        end,
+        "Harpoon File 3"
+      },
+      ["<leader>h4"] = {
+        function()
+          harpoon:list():select(4)
+        end,
+        "Harpoon File 4"
+      }
+    })
+
+    -- Additional navigation keymaps
+    vim.keymap.set("n", "<C-S-P>", function()
+      harpoon:list():prev()
+    end)
+    vim.keymap.set("n", "<C-S-N>", function()
+      harpoon:list():next()
+    end)
   end,
 }

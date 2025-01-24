d69c9c0970c59570bd6d61cfe15204e533d54a0b
Jung Hoan Kim <kjunghoan@gmail.com>
2025-01-24 00:35:50 +0000

    [Refactor] Extracting packages from file
    Moved the terminal split commands from <leader>t which overlaped with
    test runners to <leader>;

--- lua/config/globalKeymaps.lua
+++ lua/config/globalKeymaps.lua
@@ -1,4 +1,4 @@
-local opts = {noremap = true, silent = true}
+local opts = { noremap = true, silent = true }
 local km = vim.keymap.set
 
 -- Leader key
@@ -21,8 +21,8 @@ km("n", "<C-S-Left>", ":vertical resize +2<CR>", opts)
 km("n", "<C-S-Right>", ":vertical resize -2<CR>", opts)
 
 -- Split windows
-km("n", "<leader>sv", ":vsplit<CR>", {noremap = true, silent = true, desc = "Split vertically"})
-km("n", "<leader>sh", ":split<CR>", {noremap = true, silent = true, desc = "Split Horizontally"})
+km("n", "<leader>sv", ":vsplit<CR>", { noremap = true, silent = true, desc = "Split vertically" })
+km("n", "<leader>sh", ":split<CR>", { noremap = true, silent = true, desc = "Split Horizontally" })
 
 -- Wrap lines toggle
-km("n", "<leader>w", ":set wrap!<CR>", {noremap = true, silent = true, desc = "Toggle Wrap"})
+km("n", "<leader>w", ":set wrap!<CR>", { noremap = true, silent = true, desc = "Toggle Wrap" })
--- lua/plugins/tmux-nav.lua
+++ lua/plugins/tmux-nav.lua
@@ -1,102 +1,51 @@
 return {
-  {
-    "alexghergh/nvim-tmux-navigation",
-    config = function()
-      local nvim_tmux = require("nvim-tmux-navigation")
+  "alexghergh/nvim-tmux-navigation",
+  config = function()
+    local nvim_tmux = require("nvim-tmux-navigation")
 
-      nvim_tmux.setup({
-        disable_when_zoomed = true,
-        keybindings = {
-          left = "<C-h>",
-          down = "<C-j>",
-          up = "<C-k>",
-          right = "<C-l>",
-        },
-        -- Set the tmux prefix
-        tmux_prefix = "<C-s>",
-      })
+    nvim_tmux.setup({
+      disable_when_zoomed = true,
+      keybindings = {
+        left = "<C-h>",
+        down = "<C-j>",
+        up = "<C-k>",
+        right = "<C-l>",
+      },
+      -- Set the tmux prefix
+      tmux_prefix = "<C-s>",
+    })
 
-      -- Register keybindings with which-key
-      local wk = require("which-key")
-      wk.add({
-        {
-          "<C-h>",
-          function()
-            require("nvim-tmux-navigation").NvimTmuxNavigateLeft()
-          end,
-          desc = "Navigate Left",
-        },
-        {
-          "<C-j>",
-          function()
-            require("nvim-tmux-navigation").NvimTmuxNavigateDown()
-          end,
-          desc = "Navigate Down",
-        },
-        {
-          "<C-k>",
-          function()
-            require("nvim-tmux-navigation").NvimTmuxNavigateUp()
-          end,
-          desc = "Navigate Up",
-        },
-        {
-          "<C-l>",
-          function()
-            require("nvim-tmux-navigation").NvimTmuxNavigateRight()
-          end,
-          desc = "Navigate Right",
-        },
-      })
-    end,
-  },
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    config = function()
-      require("toggleterm").setup({
-        size = 20,
-        open_mapping = [[<C-\>]],
-        hide_numbers = true,
-        shade_terminals = true,
-        start_in_insert = true,
-        insert_mappings = true,
-        direction = "float",
-        close_on_exit = true,
-        shell = vim.o.shell,
-        float_opts = {
-          border = "curved",
-          winblend = 0,
-        },
-      })
-
-      -- Lazygit terminal setup
-      local Terminal = require("toggleterm.terminal").Terminal
-      local lazygit = Terminal:new({
-        cmd = "lazygit",
-        hidden = true,
-        direction = "float",
-      })
-
-      function _LAZYGIT_TOGGLE()
-        lazygit:toggle()
-      end
-
-      -- Terminal keybindings
-      local wk = require("which-key")
-      wk.add({ -- TODO wk spec
-        { "<leader>t", group = "Terminal" },
-        {
-          "<leader>tg",
-          function()
-            _LAZYGIT_TOGGLE()
-          end,
-          desc = "Lazygit",
-        },
-        { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", desc = "Float" },
-        { "<leader>th", "<cmd>ToggleTerm size=10 direction=horizontal<cr>", desc = "Horizontal" },
-        { "<leader>tv", "<cmd>ToggleTerm size=80 direction=vertical<cr>", desc = "Vertical" },
-      })
-    end,
-  },
+    -- Register keybindings with which-key
+    local wk = require("which-key")
+    wk.add({
+      {
+        "<C-h>",
+        function()
+          require("nvim-tmux-navigation").NvimTmuxNavigateLeft()
+        end,
+        desc = "Navigate Left",
+      },
+      {
+        "<C-j>",
+        function()
+          require("nvim-tmux-navigation").NvimTmuxNavigateDown()
+        end,
+        desc = "Navigate Down",
+      },
+      {
+        "<C-k>",
+        function()
+          require("nvim-tmux-navigation").NvimTmuxNavigateUp()
+        end,
+        desc = "Navigate Up",
+      },
+      {
+        "<C-l>",
+        function()
+          require("nvim-tmux-navigation").NvimTmuxNavigateRight()
+        end,
+        desc = "Navigate Right",
+      },
+    })
+  end,
 }

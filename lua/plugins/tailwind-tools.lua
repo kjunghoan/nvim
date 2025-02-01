4a3e14ed7ee52d3f947134a03d8f4cebc169744a
Jung Hoan Kim <kjunghoan@gmail.com>
2025-02-01 18:29:55 +0000

    temporarily removing tailwind

--- lua/plugins/tailwind-tools.lua
+++ lua/plugins/tailwind-tools.lua
@@ -1,41 +1,41 @@
 -- tailwind-tools.lua
 return {
-  "luckasRanarison/tailwind-tools.nvim",
-  name = "tailwind-tools",
-  build = ":UpdateRemotePlugins",
-  ft = {
-    "css",
-    "scss",
-    "sass",
-    "html",
-    "vue",
-    "svelte",
-    "astro",
-    "typescript",
-    "javascript",
-    "typescriptreact",
-    "javascriptreact",
-  },
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "telescope.nvim", -- optional
-    "nvim-lspconfig", -- optional
-  },
-  opts = {
-    server = {
-      filetypes = {
-        "css",
-        "scss",
-        "sass",
-        "html",
-        "vue",
-        "svelte",
-        "astro",
-        "typescript",
-        "javascript",
-        "typescriptreact",
-        "javascriptreact",
-      },
-    },
-  },
+  -- "luckasRanarison/tailwind-tools.nvim",
+  -- name = "tailwind-tools",
+  -- build = ":UpdateRemotePlugins",
+  -- ft = {
+  --   "css",
+  --   "scss",
+  --   "sass",
+  --   "html",
+  --   "vue",
+  --   "svelte",
+  --   "astro",
+  --   "typescript",
+  --   "javascript",
+  --   "typescriptreact",
+  --   "javascriptreact",
+  -- },
+  -- dependencies = {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   "telescope.nvim", -- optional
+  --   "nvim-lspconfig", -- optional
+  -- },
+  -- opts = {
+  --   server = {
+  --     filetypes = {
+  --       "css",
+  --       "scss",
+  --       "sass",
+  --       "html",
+  --       "vue",
+  --       "svelte",
+  --       "astro",
+  --       "typescript",
+  --       "javascript",
+  --       "typescriptreact",
+  --       "javascriptreact",
+  --     },
+  --   },
+  -- },
 }

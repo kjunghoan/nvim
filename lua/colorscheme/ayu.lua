7ddaf6c755581d2dc0e6b3d42020f856b36ad99b
Jung Hoan Kim <kjunghoan@gmail.com>
2025-02-25 07:50:59 +0000

    Visual updates to Ayu colorscheme

--- lua/colorscheme/ayu.lua
+++ lua/colorscheme/ayu.lua
@@ -6,9 +6,17 @@ return {
     config = function()
       require("ayu").setup({
         mirage = true, -- Set to `true` if you want to use the mirage variant
-        overrides = {}, -- You can add any overrides here
+        overrides = {
+          Normal = { bg = "None" },
+          NormalFloat = { bg = "none" },
+          SignColumn = { bg = "None" },
+          Folded = { bg = "None" },
+          FoldColumn = { bg = "None" },
+          CursorColumn = { bg = "None" },
+        }, -- You can add any overrides here
       })
       vim.cmd("colorscheme ayu")
     end,
   },
 }
+

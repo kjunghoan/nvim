7ef56d761be6a8f309c6a247f4145d759ab5713b
Jung Hoan Kim <kjunghoan@gmail.com>
2025-03-22 14:57:05 -0400

    ltex update

--- lua/plugins/ltex.lua
+++ lua/plugins/ltex.lua
@@ -6,83 +6,174 @@ return {
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
     },
-    ft = { "markdown", "tex", "text", "mail" }, -- Add file types you want LTeX to check
+    ft = { "markdown", "tex", "text", "mail" },
     config = function()
-      -- Configure ltex language server
+      local ltex_extra = require("ltex_extra")
+
+      local ignore_words = {
+        ["en-US"] = {},
+        ["en-GB"] = {},
+        ["ko"] = {},
+      }
+
+      local last_check_time = 0
+      local check_throttle_ms = 2000 -- Only check every 2 seconds
+
+      local function throttled_check(check_func)
+        return function(...)
+          local current_time = vim.loop.now()
+          if current_time - last_check_time > check_throttle_ms then
+            last_check_time = current_time
+            return check_func(...)
+          end
+          return false
+        end
+      end
+
       require("lspconfig").ltex.setup({
         on_attach = function(client, bufnr)
-          -- Configure ltex_extra dictionary management
-          local ltex_extra = require("ltex_extra")
           ltex_extra.setup({
-            load_langs = { "en-US", "en-GB", "ko" }, -- Preload dictionaries for these languages
-            init_check = true, -- Check on startup
-            path = vim.fn.stdpath("config") .. "/spell", -- Store dictionaries in the spell directory
-            log_level = "none",
+            load_langs = { "en-US", "en-GB", "ko" },
+            init_check = false, -- Disable initial check
+            path = vim.fn.stdpath("config") .. "/spell",
+            log_level = "error",
           })
-          
-          -- Get which-key for keybinding registration
-          local wk = require("which-key")
-          
-          -- Function to change LTeX language
-          local function change_ltex_language()
-            vim.ui.select(
-              { "en-US", "en-GB", "ko" }, 
-              { prompt = "Select language:" }, 
-              function(choice)
-                if choice then
-                  -- Update the language setting for the current buffer
-                  client.config.settings.ltex.language = choice
-                  -- Apply the new settings to the client
-                  client.notify('workspace/didChangeConfiguration', {
-                    settings = client.config.settings
-                  })
-                  vim.notify("LTeX language changed to " .. choice, vim.log.levels.INFO)
-                end
+
+          for lang, words in pairs(ignore_words) do
+            for _, word in ipairs(words) do
+              if not client.config.settings.ltex.dictionary[lang] then
+                client.config.settings.ltex.dictionary[lang] = {}
               end
-            )
+              table.insert(client.config.settings.ltex.dictionary[lang], word)
+            end
           end
-          
-          -- Register additional keybindings using LSP code actions
-          wk.register({
-            ["<leader>ly"] = { 
-              name = "LTeX", 
-              a = { "<cmd>lua vim.lsp.buf.code_action()<CR>", "LTeX Code Actions" },
-              r = { function() 
-                -- Reload the ltex_extra dictionaries
-                ltex_extra.setup({
-                  load_langs = { "en-US", "en-GB", "ko" },
-                  init_check = true,
-                  path = vim.fn.stdpath("config") .. "/spell",
+
+          client.notify("workspace/didChangeConfiguration", {
+            settings = client.config.settings,
+          })
+
+          local wk = require("which-key")
+
+          local function change_ltex_language()
+            vim.ui.select({ "en-US", "en-GB", "ko" }, { prompt = "Select language:" }, function(choice)
+              if choice then
+                client.config.settings.ltex.language = choice
+                client.notify("workspace/didChangeConfiguration", {
+                  settings = client.config.settings,
                 })
-                vim.notify("LTeX dictionaries reloaded", vim.log.levels.INFO)
-              end, "Reload LTeX Dictionaries" },
+                vim.notify("LTeX language changed to " .. choice, vim.log.levels.INFO)
+              end
+            end)
+          end
+
+          wk.register({
+            ["<leader>ly"] = {
+              name = "LTeX",
+              a = { "<cmd>lua vim.lsp.buf.code_action()<CR>", "LTeX Code Actions" },
+              r = {
+                function()
+                  ltex_extra.setup({
+                    load_langs = { "en-US", "en-GB", "ko" },
+                    init_check = true,
+                    path = vim.fn.stdpath("config") .. "/spell",
+                  })
+                  vim.notify("LTeX dictionaries reloaded", vim.log.levels.INFO)
+                end,
+                "Reload LTeX Dictionaries",
+              },
               l = { change_ltex_language, "Switch language" },
+              d = {
+                function()
+                  local current_state = client.config.settings.ltex.enabled
+                  client.config.settings.ltex.enabled = not current_state
+                  client.notify("workspace/didChangeConfiguration", {
+                    settings = client.config.settings,
+                  })
+                  vim.notify("LTeX " .. (not current_state and "enabled" or "disabled"), vim.log.levels.INFO)
+                end,
+                "Toggle LTeX",
+              },
             },
           }, { buffer = bufnr })
         end,
-        
+
         settings = {
           ltex = {
-            -- Configure multiple language variants
-            language = "en-US", -- Default language
+            enabled = true,
+            language = "en-US",
+            checkFrequency = "save",
+            logLevel = "severe",
             additionalRules = {
-              enablePickyRules = true, -- Enable picky rules for stricter checking
+              enablePickyRules = false,
+              motherTongue = "en-US",
             },
-            -- Dictionaries for the configured languages
+            disabledRules = {
+              ["en-US"] = {
+                "PROFANITY",
+                "EN_QUOTES",
+              },
+              ["en-GB"] = {
+                "PROFANITY",
+                "EN_QUOTES",
+              },
+              ["ko"] = {},
+            },
             dictionary = {
-              ["en-US"] = {}, -- Will be populated by ltex_extra
-              ["en-GB"] = {}, -- Will be populated by ltex_extra
-              ["ko"] = {},    -- Will be populated by ltex_extra
+              ["en-US"] = {},
+              ["en-GB"] = {},
+              ["ko"] = {},
             },
-            -- Words to disable checking for
-            disabledRules = {},
-            hiddenFalsePositives = {},
-            -- Configure LTeX more precisely
-            checkFrequency = "edit", -- Check on edit (alternatives: "save", "manual")
-            diagnosticSeverity = "information", -- Set diagnostic severity
+            markdown = {
+              nodes = {
+                CodeBlock = "ignore",
+                FencedCodeBlock = "ignore",
+                AutoLink = "ignore",
+                Link = "ignore",
+                WikiLink = "ignore",
+              },
+            },
+            diagnosticSeverity = "information",
+            hiddenFalsePositives = {
+              ["en-US"] = [=[
+              "Alki"
+              ]=],
+              ["en-GB"] = [=[
+              "Alki"
+              ]=],
+              ["ko"] = [=[
+              ]=],
+            },
           },
         },
+        flags = {
+          debounce_text_changes = 2000, -- Debounce for 2 seconds
+          allow_incremental_sync = false,
+        },
+        init_options = {
+          serverStatusNotification = true,
+          documentChunkSize = 5000,
+        },
+      })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "markdown", "tex", "text", "mail" },
+        callback = function()
+          vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePost" }, {
+            buffer = 0, -- current buffer
+            callback = function()
+              local clients = vim.lsp.get_active_clients({ bufnr = 0 })
+              for _, client in ipairs(clients) do
+                if client.name == "ltex" then
+                  vim.schedule(function()
+                    vim.diagnostic.show()
+                  end)
+                  break
+                end
+              end
+            end,
+          })
+        end,
       })
     end,
-  }
+  },
 }

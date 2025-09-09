54426c3ba21e8ce180404955adedf11d3c7b3421
Jung Hoan Kim <kjunghoan@gmail.com>
2025-09-09 02:03:29 +0000

    kubectl update

--- lua/plugins/kubectl.lua
+++ lua/plugins/kubectl.lua
@@ -1,36 +1,34 @@
 return {
   'Ramilito/kubectl.nvim',
+  dependencies = { "saghen/blink.download", },
   config = function()
-    require("kubectl").setup()
+    require("kubectl").setup({
+      kubectl_cmd = { 
+        cmd = "kubectl",
+        env = {
+          KUBECONFIG = vim.fn.expand("~/.kube/config")
+        }
+      }
+    })
+    -- Override helm command path
+    local helm_def = require("kubectl.resources.helm")
+    if helm_def and helm_def.definition then
+      helm_def.definition.cmd = "/home/linuxbrew/.linuxbrew/bin/helm"
+    end
   end,
+  namespace = "All",
   cmd = { 'Kubectl', 'Kubectx', 'Kubens' },
   keys = {
     { '<leader>k', '<cmd>lua require("kubectl").toggle()<cr>' },
+    { "<C-t>",     "<Plug>(kubectl.view_top)",                ft = "k8s_*" },
+    { "<C-k>",     "<Plug>(kubectl.kill)",                    ft = "k8s_*" },
+    { "7",         "<Plug>(kubectl.view_nodes)",              ft = "k8s_*" },
+    { "8",         "<Plug>(kubectl.view_events)",             ft = "k8s_*" },
+    { "9",         "<Plug>(kubectl.view_pv)",                 ft = "k8s_*" },
+    { "0",         "<Plug>(kubectl.view_helm)",               ft = "k8s_*" },
+    { "<F2>",      "<Plug>(kubectl.view_api_resources)",      ft = "k8s_*" },
+    { "<F3>",      "<Plug>(kubectl.view_crds)",               ft = "k8s_*" },
+    { "<F4>",      "<Plug>(kubectl.view_cronjobs)",           ft = "k8s_*" },
+    { "<F5>",      "<Plug>(kubectl.view_pvc)",                ft = "k8s_*" },
   },
-  init = function()
-    local group = vim.api.nvim_create_augroup("kubectl_custom_mappings", { clear = true })
-    vim.api.nvim_create_autocmd("FileType", {
-      group = group,
-      pattern = "k8s_*",
-      callback = function(ev)
-        local k = vim.keymap.set
-        local opts = { buffer = ev.buf }
-
-        k("n", "<C-t>", "<Plug>(kubectl.view_top)", opts)
-        k("n", "<C-k>", "<Plug>(kubectl.kill)", opts)
-        k("n", "7", "<Plug>(kubectl.view_nodes)", opts)
-
-        k("n", "8", "<Plug>(kubectl.view_events)", opts)
-        k("n", "9", "<Plug>(kubectl.view_pv)", opts)
-        -- k("n", "0", "<Plug>(kubectl.view_helm)", opts)
-
-        k("n", "<F2>", "<Plug>(kubectl.view_api_resources)", opts)
-        k("n", "<F3>", "<Plug>(kubectl.view_crds)", opts)
-        k("n", "<F4>", "<Plug>(kubectl.view_cronjobs)", opts)
-        k("n", "<F5>", "<Plug>(kubectl.view_pvc)", opts)
-        -- k("n", "9", "<Plug>(kubectl.view_sa)", opts)
-        -- k("n", "9", "<Plug>(kubectl.view_overview)", opts)
-      end
-    })
-  end,
 }

23cd1d29965a2d7cabe0cc41f113a8bdbc51c8df
Jung Hoan Kim <kjunghoan@gmail.com>
2025-11-22 12:17:06 -0500

    updates to render-markdown inline tex

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -3,7 +3,17 @@ return {
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- If you use the mini.nvim suite
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' }, -- If you use standalone mini plugins
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- If you prefer nvim-web-devicons
-  opts = {},
+  opts = {
+    latex = {
+      enabled = true,
+      render_modes = false,
+      converter = {'utftex','latex2text'},
+      highlight = 'RenderMarkdownMath',
+      position = 'center',
+      top_pad = 1,
+      bottom_pad = 1,
+    }
+  },
   config = function(_, opts)
     require('render-markdown').setup(opts)
   end,

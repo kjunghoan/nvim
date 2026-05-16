388dbd0702c6d30ade8b76fcf73400b5f1c5e0e3
Simon JungHoan Kim <kjunghoan@gmail.com>
2026-05-16 15:05:51 -0400

    pre-change stuff 2026-05-16

--- after/lsp/ts_ls.lua
+++ after/lsp/ts_ls.lua
@@ -1,4 +1,7 @@
 -- TypeScript/JavaScript language server configuration
+local vue_plugin_path = vim.fn.stdpath("data")
+  .. "/mason/packages/vue-language-server/node_modules/@vue/typescript-plugin"
+
 return {
   cmd = { "typescript-language-server", "--stdio" },
   filetypes = {
@@ -6,8 +9,18 @@ return {
     "javascriptreact",
     "typescript",
     "typescriptreact",
+    "vue",
   },
   root_markers = { "package.json", "tsconfig.json", "jsconfig.json", ".git" },
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = vue_plugin_path,
+        languages = { "vue" },
+      },
+    },
+  },
   settings = {
     typescript = {
       inlayHints = {
